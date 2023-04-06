8efad6d65445b9fba313a71cff0b3a707cb4465e
James Jelenko <jamesjelenko@gmail.com>
2023-04-06 17:01:09 -0600

    Initial: add function skeletons, create initial test for transcription function

---
+++ my_utils/audio/audio.py
@@ -0,0 +1,13 @@
+import openai, os
+
+
+MODEL_NAME = 'whisper-1'
+
+def transcribe_audio(directory=None):
+    for file in os.listdir(directory):
+        f = os.path.join(directory, file)
+        if os.path.isfile(f):
+             audio_file = open(f, 'rb')
+             transcript = openai.Audio.transcribe(MODEL_NAME, audio_file)
+             return transcript
+
---
+++ my_utils/images/image.py
@@ -0,0 +1,7 @@
+import openai, os 
+
+
+def create_image(**kwargs):	
+ 	 """ # TODO Check for mechanism to ensure keyword args fit expected terms, e.g. "prompt","size", and not "fubar"
+ 	 Kwargs includes: prompt=<string>, n=<int>, size=<string> \n Example: openai.Images.create(prompt='description of image', n=3, size="512x512") """
+# TODO Check for mechanism to ensure keyword args fit expected terms, e.g. "prompt","size", and not "fubar"
