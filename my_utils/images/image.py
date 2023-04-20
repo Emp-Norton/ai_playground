93d6018020f8f6340418ded5e95d020be91728b0
James Jelenko <jamesjelenko@gmail.com>
2023-04-19 18:39:09 -0700

    IMAGES: add todo about kwarg validation, add docstring

--- my_utils/images/image.py
+++ my_utils/images/image.py
@@ -2,8 +2,8 @@ import openai, os
 
 
 def create_image(**kwargs):	
- 	 """ # TODO Check for mechanism to ensure keyword args fit expected terms, e.g. "prompt","size", and not "fubar"
- 	 Kwargs includes: prompt=<string>, n=<int>, size=<string> \n Example: openai.Images.create(prompt='description of image', n=3, size="512x512") """
+""" # TODO Check for mechanism to ensure keyword args fit expected terms, e.g. "prompt","size", and not "fubar"
+Kwargs includes: prompt=<string>, n=<int>, size=<string> \n Example: openai.Images.create(prompt='description of image', n=3, size="512x512") """
 # TODO Check for mechanism to ensure keyword args fit expected terms, e.g. "prompt","size", and not "fubar"
 # TODO Add option to toggle API return between URL and Base64 PNG
 # TODO Add logging and save to file function for PNG
