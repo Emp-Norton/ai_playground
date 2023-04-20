bd938db70f68e22e674ae9fe5e3ba3a538065ead
James Jelenko <jamesjelenko@gmail.com>
2023-04-19 18:39:48 -0700

    MAIN: add basic transcribe function, remove old leftover code

--- my_utils/openai_api_textspeach.py
+++ my_utils/openai_api_textspeach.py
@@ -1,11 +1,17 @@
 import os
 import openai
-import env 
 
 
-def create_image(**kwargs):	
- 	 """ Create an image or series of images fitting a given description (i.e. prompt). Kwargs includes: prompt=<string>, n=<int>, size=<string> \n Example: openai.Images.create(prompt='description of image', n=3, size="512x512") """
-# TODO Check for mechanism to ensure keyword args fit expected terms, e.g. "prompt","size", and not "fubar"
+def transcribe(VID_DIR):
+    if VID_DIR is not None:
+        try:
+            for file in os.listdir(VID_DIR):
+                f = os.path.join(VID_DIR, file)
+                if os.path.isfile(f):
+                    audio_file = open(f, 'rb')
+                    transcript = openai.Audio.transcribe('whisper-1', audio_file)
+                    print(transcript)
+                    print('-------------------------------\n\n')
+        except Exception as e:
+            print('Failed with {e} on file {file}')
 
-def transcribe_audio():
-	""" """"
