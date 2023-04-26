6bb0d4701f1418c2718b93f97fd130a17fae394a
James Jelenko <jamesjelenko@gmail.com>
2023-04-25 20:06:25 -0700

    AUDIO: update constants file with language strings

--- my_utils/audio/language_constants.py
+++ my_utils/audio/language_constants.py
@@ -1,1 +1,7 @@
+""" This classes that comprise this file contain the hardcoded language names as strings, to ensure no typo-related errors, and to maintain a single source of truth and updates. New languages and terms should be added here."""
+
+class Languages():
 ENGLISH = 'english'
+PORTUGUESE = 'portuguese'
+FRENCH = 'french'
+SPANISH = 'spanish'
